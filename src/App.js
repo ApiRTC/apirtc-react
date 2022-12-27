7c31128b707924d11e7fb5bdcb35a5de45ed129c
Romain <romain.vailleux@gmail.com>
2022-12-27 11:30:28 +0100

    new version of the tutorial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from "react"
+import ConversationComponent from "./Components/ConversationComponent"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ConversationComponent></ConversationComponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
